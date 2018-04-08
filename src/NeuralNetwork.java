58738f0260853062747a8bddbe4260a81436fa2c
ilya_litosh <ilya_litosh@hotmail.com>
2018-04-08 21:22:02 +0300

    123

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -11,12 +11,6 @@ public class NeuralNetwork {
     private static double[] inputNeurals = new double[160000];                              //массив входных нейронов
     private static ArrayList<Double> outputNeurals = new ArrayList<>();                     //массив выходных нейронов
     private static ArrayList<String> recognizableResult = new ArrayList<>();
-    private final static double EXPECTED_VALUE = 1.0;
-    private static double error;
-    private static double weightsDelta; // корректирующее значение весов
-    private final static double LEARNING_RATE = 0.001; // коэффициент скорости обучения
-    Raster raster;
-    ColorModel model;
 
     double[] getInputNeurals(){
 
@@ -70,12 +64,6 @@ public class NeuralNetwork {
         loadMemory();
     }
 
-    /** функция активации(сигмоида) */
-    private double activation(double value){
-
-        return (1/(1 + Math.exp(-value)));
-    }
-
     /** выходные данные выходных нейронов */
     void computeOutputDataNeuralNetwork(){
         for(int i = 0; i < outputNeurals.size(); i++){
@@ -98,64 +86,6 @@ public class NeuralNetwork {
             }
         }
         return getRecognizableSymbols().get(indexOfMax);
-    }
-
-    /** тренировка сети */
-    private void training(String nameOfNeural){
-        if(nameOfNeural.equals("")){
-            System.out.println("Error11! Не введено название обучаемого объекта");
-            return;
-        }
-        int numberOfNeural = 0;                                         // номер нейрона
-        for(int i = 0; i < recognizableResult.size(); i++){             // определение номера нейрона по названию распозноваемого объекта
-            if(recognizableResult.get(i).equals(nameOfNeural)){
-                numberOfNeural = i;
-                break;
-            }
-            if(i == (recognizableResult.size() - 1)){
-                for(int j = 0; j < 160000; j++){
-                    weights.add(.0);
-                }
-                recognizableResult.add(nameOfNeural);
-                outputNeurals.add(.0);
-            }
-        }
-        error = EXPECTED_VALUE - activation(outputNeurals.get(numberOfNeural));
-        weightsDelta = error * (activation(outputNeurals.get(numberOfNeural))*(1 - activation(outputNeurals.get(numberOfNeural))));
-        System.out.println(weightsDelta + "------");
-        for(int i = 0; i < recognizableResult.size(); i++){
-            if(recognizableResult.get(i).equals(nameOfNeural)){
-                for(int j = 0; j < inputNeurals.length; j++){
-                    weights.set(j + (i*160000), weights.get(j + (i*160000)) + inputNeurals[j] * weightsDelta * LEARNING_RATE);
-                }
-                break;
-            }
-        }
-        System.out.println("тренировка завершина");
-        updateMemory();
-    }
-
-    /** вывод данных после пробега через функцию активации(результат расчета нейросети) */
-    private void showOutput(){
-        for(int i = 0; i < recognizableResult.size(); i++){
-            System.out.println(recognizableResult.get(i) + " : \t" + activation(outputNeurals.get(i)));
-        }
-    }
-
-    /** сохраняем(обновляем) память нейросети */
-    private void updateMemory(){
-        try{
-            FileWriter writeInFile = new FileWriter(new File("src/memory/memory.crn"));
-            writeInFile.write(weights.toString());
-            writeInFile.flush();
-            writeInFile.close();
-        }catch (IOException e){}
-        try{
-            FileWriter writeInFile = new FileWriter(new File("src/memory/learnedObjects.crn"));
-            writeInFile.write(recognizableResult.toString());
-            writeInFile.flush();
-            writeInFile.close();
-        }catch (IOException e){}
     }
 
     private void loadMemory(){
